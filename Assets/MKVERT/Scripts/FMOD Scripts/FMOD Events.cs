2857265c824f436fb1c9b8845e98252f3f1fc1f3
althom12 <109119094+althom12@users.noreply.github.com>
2025-03-01 05:53:00 +0000

    Modified Audio Manager and FMODEvent scripts
    Modified Audio Manager and FMODEvent scripts

--- Assets/MKVERT/Scripts/FMOD Scripts/FMOD Events.cs
+++ Assets/MKVERT/Scripts/FMOD Scripts/FMOD Events.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class FMODEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: Header("Ambience")]
+    [field: SerializeField] public EventReference ambience { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+    public static FMODEvents instance {get; private set;}
+
+    private void Awake()
     {
-        
-    }
+      if (instance != null)
+      {
+         Debug.Log("Found more than one FMOD Events instance in scene");
+      } 
+      instance = this;
+   }
 }
